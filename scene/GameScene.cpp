0fdd52f6899bbbe6cbfd9f76d2da61cc2d743a18
KeroppiKun <fuga0809ace@gmail.com>
2023-04-17 16:01:46 +0900

    Revert "数学関数移植"
    This reverts commit 9fd9a09f0c197f7b1d7b5bdf3ab60c88044a2eb5.

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,67 +1,19 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-#include "ImGuiManager.h"
-#include "PrimitiveDrawer.h"
-#include "AxisIndicator.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() { 
-
-	delete sprite_;
-	delete model_;
-	delete debugCamera_;
-
-}
+GameScene::~GameScene() {}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
-	textureHandle_ = TextureManager::Load("mario.jpg");
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-	model_ = Model::Create();
-	worldTransform_.Initialize();
-	viewProjection_.Initialize();
-	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
-	audio_->PlayWave(soundDataHandle_);
-	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
-	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
-	debugCamera_ = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight);
-	AxisIndicator::GetInstance()->SetVisible(true);
-	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {
-
-	//スプライトの現在座標取得
-	Vector2 position = sprite_->GetPosition();
-	//座標を(2,1)移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
-
-	if (input_->TriggerKey(DIK_SPACE)) {
-		//音声再生
-		audio_->StopWave(voiceHandle_);
-	}
-
-	
-	ImGui::Begin("Debug1");
-	//float3入力ボックス
-	ImGui::InputFloat3("InputFloat3", inputFloat3);
-	//float3スライダー
-	ImGui::SliderFloat3("SliderFloat3",inputFloat3,0.0f,1.0f);
-	ImGui::End();
-
-	ImGui::ShowDemoWindow();
-
-	debugCamera_->Update();
-
-}
+void GameScene::Update() {}
 
 void GameScene::Draw() {
 
@@ -75,9 +27,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-
-	//sprite_->Draw();
-	
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
@@ -93,11 +42,8 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
-	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
-
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
-	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
 #pragma endregion
 
 #pragma region 前景スプライト描画
